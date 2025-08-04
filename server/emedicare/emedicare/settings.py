3b4cc977b13b625e2c782aecadd7dedd65626c93
Sandhya <sandhyach062@gmail.com>
2025-08-04 22:23:37 +0545

    Initial commit

--- server/emedicare/emedicare/settings.py
+++ server/emedicare/emedicare/settings.py
@@ -68,7 +68,7 @@ CORS_ALLOW_HEADERS = [
 # Application definition
 
 INSTALLED_APPS = [
-    # 'jazzmin',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
